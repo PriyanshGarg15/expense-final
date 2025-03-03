bf6c3d2de1b0efa9728fea3c28549871aab04ebd
PriyanshGarg15 <116974262+PriyanshGarg15@users.noreply.github.com>
2025-03-04 00:35:30 +0530

    Update connectDB.js

--- config/connectDB.js
+++ config/connectDB.js
@@ -6,7 +6,7 @@ mongoose.set('strictQuery', false);
 const connectDb = async() => {
     try{
         // await mongoose.connect("mongodb+srv://up:qwert@cluster0.wbcf5.mongodb.net/main_final_expense"),
-        await mongoose.connect("mongodb://priyanshgargcs22:3DDR44i7gZXuYfP3@undefined/?replicaSet=atlas-393z4y-shard-0&ssl=true&authSource=admin"),
+        await mongoose.connect("mongodb://priyanshgargcs22:3DDR44i7gZXuYfP3/?replicaSet=atlas-393z4y-shard-0&ssl=true&authSource=admin"),
         console.log(`Server running On ${mongoose.connection.host}`.bgCyan.white)
     }
     catch(error){
