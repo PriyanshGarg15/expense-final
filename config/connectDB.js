40ae1a2319bd780c986e32fd8cda03e78f883ea7
PriyanshGarg15 <116974262+PriyanshGarg15@users.noreply.github.com>
2025-03-04 00:44:35 +0530

    Update connectDB.js

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,19 +1,37 @@
+// const mongoose = require('mongoose');
+// const colors = require('colors');
+// mongoose.set('strictQuery', false);
+
+
+// const connectDb = async() => {
+//     try{
+//         // await mongoose.connect("mongodb+srv://up:qwert@cluster0.wbcf5.mongodb.net/main_final_expense"),
+//         // await mongoose.connect("mongodb://priyanshgargcs22:3DDR44i7gZXuYfP3/?replicaSet=atlas-393z4y-shard-0&ssl=true&authSource=admin"),
+//         await mongoose.connect("mongodb+srv://priyanshgargcs22:3DDR44i7gZXuYfP3@your-cluster.mongodb.net/your-database-name?retryWrites=true&w=majority")
+
+//         console.log(`Server running On ${mongoose.connection.host}`.bgCyan.white)
+//     }
+//     catch(error){
+//         console.log(`${error}`.bgRed)
+//     }
+// }
+
+// module.exports =connectDb
 const mongoose = require('mongoose');
 const colors = require('colors');
 mongoose.set('strictQuery', false);
 
+const connectDb = async () => {
+    try {
+        await mongoose.connect("mongodb://priyanshgargcs22:3DDR44i7gZXuYfP3/?replicaSet=atlas-393z4y-shard-0&ssl=true&authSource=admin", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
 
-const connectDb = async() => {
-    try{
-        // await mongoose.connect("mongodb+srv://up:qwert@cluster0.wbcf5.mongodb.net/main_final_expense"),
-        // await mongoose.connect("mongodb://priyanshgargcs22:3DDR44i7gZXuYfP3/?replicaSet=atlas-393z4y-shard-0&ssl=true&authSource=admin"),
-        await mongoose.connect("mongodb+srv://priyanshgargcs22:3DDR44i7gZXuYfP3@your-cluster.mongodb.net/your-database-name?retryWrites=true&w=majority")
-
-        console.log(`Server running On ${mongoose.connection.host}`.bgCyan.white)
+        console.log(`Server running On ${mongoose.connection.host}`.bgCyan.white);
+    } catch (error) {
+        console.log(`${error}`.bgRed);
     }
-    catch(error){
-        console.log(`${error}`.bgRed)
-    }
-}
+};
 
-module.exports =connectDb
+module.exports = connectDb;
